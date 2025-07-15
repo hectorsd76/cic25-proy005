43e28abedf5945ee50cab750133c0184b777709b
hsolana <hsolana@formacion.cic.es>
2025-07-15 12:24:05 +0200

    Resto de métodos del CRUD y un ejemplo de test para el delete

--- src/main/java/es/cic/curso2025/proy005/TelefonoController.java
+++ src/main/java/es/cic/curso2025/proy005/TelefonoController.java
@@ -1,16 +1,81 @@
 package es.cic.curso2025.proy005;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 
 @RestController
 @RequestMapping("/telefono")
 public class TelefonoController {
 
-    @GetMapping
-    public String mensaje(){
-        return "Hola buenas";
+    private int contador = 0;
+
+    // C R U D
+
+    @PostMapping
+    public long create(@RequestBody Telefono telefono){
+        return ++contador;
+
+
     }
+
+    @GetMapping
+    public List<Telefono> get(){
+        // Simular que devuelve una lista de telf
+        List<Telefono> resultado = new ArrayList<>();
+
+        Telefono telefono1 = new Telefono();
+        telefono1.setId(1);
+        telefono1.setNumero("54");
+        telefono1.setTipoContrato(2);
+        telefono1.setTitular("Héctor");
+        
+        resultado.add(telefono1);
+
+
+        Telefono telefono2 = new Telefono();
+        telefono2.setId(2);
+        telefono2.setNumero("43");
+        telefono2.setTipoContrato(1);
+        telefono2.setTitular("Solana");
+        
+        resultado.add(telefono2);
+
+
+        return resultado ;
+    }
+
+
+    @GetMapping("{id}")
+    public Telefono get(@PathVariable long id){
+
+        Telefono telefono1 = new Telefono();
+        telefono1.setId(id);
+        telefono1.setNumero("54");
+        telefono1.setTipoContrato(2);
+        telefono1.setTitular("Hector");
+        
+      return telefono1;
+    }
+
+    @PutMapping
+    public void update (@RequestBody Telefono telefono) {
+        // TODO
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        contador = contador;
+    }
+
 
 }
--- src/test/java/es/cic/curso2025/proy005/Proy005ApplicationTests.java
+++ src/test/java/es/cic/curso2025/proy005/Proy005ApplicationTests.java
@@ -1,13 +1,48 @@
 package es.cic.curso2025.proy005;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Proy005ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+
+	/**
+	 * 
+	 */
 	@Test
-	void contextLoads() {
+	void testTelefonoDelete() throws Exception {
+		mockMvc
+		.perform(delete("/telefono/4"))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andReturn();
+
+	}
+
+	@Test
+	void testLista(){
+
+		// Manera correcta para asignar el tipo
+		ArrayList<String> miLista = new ArrayList<>();
+		miLista.add("Hola");
+		String mensajeOriginal = miLista.get(0);
 	}
 
 }
